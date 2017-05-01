30a3b2d3cd4d6d6a707225d4b30fab38ee734a95
Alexandra Nantel <alexandra@deuxhuithuit.com>
2017-05-01 16:53:54 -0400

    Fix compatibility php 7 strict mode.

--- data-sources/data.mobiledevicedetection.php
+++ data-sources/data.mobiledevicedetection.php
@@ -1,14 +1,14 @@
 <?php
-	
+
 	require_once TOOLKIT . '/class.datasource.php';
-	
+
 	class DataSourceMobileDeviceDetection extends Datasource {
 		public function __construct($parent, $env = null, $process_params = true) {
 			parent::__construct($parent, $env, $process_params);
-			
+
 			$this->_dependencies = array();
 		}
-		
+
 		public function about() {
 			return array(
 				'name'			=> 'Mobile Device Detection',
@@ -21,32 +21,32 @@
 				'release-date'	=> '2010-01-28'
 			);
 		}
-		
+
 		public function allowEditorToParse() {
 			return false;
 		}
-		
-		public function execute($param_pool) {
+
+		public function execute(array &$param_pool = null) {
 			$data = MobileDetector::detect();
 			$result = new XMLElement('device');
 			$result->setAttribute('is-mobile', 'no');
-			
+
 			if ($data->passed()) $result->setAttribute('is-mobile', 'yes');
-			
+
 			foreach ($data->devices() as $type => $values) {
 				if (!$values->detected) continue;
-				
+
 				$item = new XMLElement($type);
-				
+
 				foreach ($values->captures as $name => $value) {
 					$item->setAttribute($name, $value);
 				}
-				
+
 				$result->appendChild($item);
 			}
-			
+
 			return $result;
 		}
 	}
-	
+
 ?>
